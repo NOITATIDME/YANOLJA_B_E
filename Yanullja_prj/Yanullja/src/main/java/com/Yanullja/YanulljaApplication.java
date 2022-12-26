0e94009afba26e0bd16ea559401fba38a866094f
CinLee <ekfhddl8@naver.com>
2022-12-26 22:59:42 +0900

    Add files via upload

---
+++ Yanullja_prj/Yanullja/src/main/java/com/Yanullja/YanulljaApplication.java
@@ -0,0 +1,21 @@
+package com.Yanullja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
+public class YanulljaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanulljaApplication.class, args);
+		home();
+	}
+
+	static void home() {
+		System.out.println("...............................................");
+		System.out.println(".................... Start ....................");
+		System.out.println("...............................................");
+	}
+}
---
+++ Yanullja_prj/Yanullja/src/test/java/com/Yanullja/YanulljaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yanullja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanulljaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
